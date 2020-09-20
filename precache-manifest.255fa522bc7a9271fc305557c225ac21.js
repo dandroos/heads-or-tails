2ef4ec5a2a640dcc63dd80e535358bfea5d3400c
David Andrews <dandrewsuk82@gmail.com>
2020-09-20 11:57:29 +0100

    Updates

---
+++ precache-manifest.255fa522bc7a9271fc305557c225ac21.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a2f6536d857c860275ea23346fa3ce3",
+    "url": "/heads-or-tails/index.html"
+  },
+  {
+    "revision": "dfb5dd023a3006d73dc1",
+    "url": "/heads-or-tails/static/js/2.a6c66c00.chunk.js"
+  },
+  {
+    "revision": "5b55082f30ebc731dc9ce1ede8e652fa",
+    "url": "/heads-or-tails/static/js/2.a6c66c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42369134484ca9464955",
+    "url": "/heads-or-tails/static/js/main.5e47bdbc.chunk.js"
+  },
+  {
+    "revision": "f5a4ef261e90b4389985",
+    "url": "/heads-or-tails/static/js/runtime-main.523f5068.js"
+  },
+  {
+    "revision": "183cc9df0b63f14c6dee51616d86d8de",
+    "url": "/heads-or-tails/static/media/coinflip.183cc9df.mp3"
+  },
+  {
+    "revision": "f7c569dc15e25e9e013ea7caab0e541c",
+    "url": "/heads-or-tails/static/media/heads.f7c569dc.png"
+  },
+  {
+    "revision": "4bd6b1aec82d84f77cfe5bca9ba36108",
+    "url": "/heads-or-tails/static/media/tails.4bd6b1ae.png"
+  }
+]);
